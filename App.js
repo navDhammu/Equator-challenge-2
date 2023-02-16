42d8c0dff547c92db76164c023c3682a911fe98f
Navdeep <nav@Navdeeps-MacBook-Air.local>
2023-02-16 09:29:00 -0500

    setup with ssr

---
+++ App.js
@@ -0,0 +1,39 @@
+import React, { useMemo } from 'react';
+import MaterialReactTable from 'material-react-table';
+import { Container, Link } from '@mui/material';
+
+export default function App({ data }) {
+	const columns = useMemo(
+		() => [
+			{
+				accessorKey: 'name',
+				header: 'Scraper Name',
+			},
+			{
+				accessorKey: 'url',
+				header: 'Github Url',
+				Cell: ({ cell }) => (
+					<Link target='_blank' href={`${cell.getValue()}`}>
+						{cell.getValue()}
+					</Link>
+				),
+			},
+			{
+				accessorKey: 'download_url', //normal accessorKey
+				header: 'Download Url',
+				Cell: ({ cell }) => (
+					<Link target='_blank' href={`${cell.getValue()}`}>
+						{cell.getValue()}
+					</Link>
+				),
+			},
+		],
+		[]
+	);
+
+	return (
+		<Container maxWidth='lg'>
+			<MaterialReactTable data={data} columns={columns} />
+		</Container>
+	);
+}
---
+++ server.js
@@ -0,0 +1,73 @@
+import express from 'express';
+import * as React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import { CacheProvider } from '@emotion/react';
+import createEmotionServer from '@emotion/server/create-instance';
+import createEmotionCache from './createEmotionCache';
+import App from './App';
+import theme from './theme';
+
+function renderFullPage(html, css, data) {
+	return `
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <title>Equator Challenge 2</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+        <meta name="emotion-insertion-point" content="" />
+        ${css}
+        <script>window.__initialData__ = ${JSON.stringify(data)} </script>
+      </head>
+      <body>
+        <script async src="build/bundle.js"></script>
+        <div id="root">${html}</div>
+      </body>
+    </html>
+  `;
+}
+
+const app = express();
+
+app.use('/build', express.static('build'));
+
+app.use(async (req, res) => {
+	try {
+		const response = await fetch(
+			'https://api.github.com/repos/Equator-Studios/scrapers/contents/scrapers'
+		);
+		const data = (await response.json()).map((scraper) => ({
+			name: scraper.name,
+			url: scraper.html_url,
+			download_url: scraper.download_url,
+		}));
+
+		const cache = createEmotionCache();
+		const { extractCriticalToChunks, constructStyleTagsFromChunks } =
+			createEmotionServer(cache);
+
+		const html = ReactDOMServer.renderToString(
+			<CacheProvider value={cache}>
+				<ThemeProvider theme={theme}>
+					<CssBaseline />
+					<App data={data} />
+				</ThemeProvider>
+			</CacheProvider>
+		);
+
+		const emotionChunks = extractCriticalToChunks(html);
+		const emotionCss = constructStyleTagsFromChunks(emotionChunks);
+
+		res.send(renderFullPage(html, emotionCss, data));
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+	console.log(`Listening on ${port}`);
+});
