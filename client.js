4d4af9ca9001b795c43e5c47ddf055df1517e276
Navdeep <nav@Navdeeps-Air.hitronhub.home>
2023-02-16 21:43:26 -0500

    fix typo

--- client.js
+++ client.js
@@ -4,7 +4,7 @@ import CssBaseline from '@mui/material/CssBaseline'
 import { ThemeProvider } from '@mui/material/styles'
 import { CacheProvider } from '@emotion/react'
 import App from './App'
-import theme from './theme.mjs'
+import theme from './theme.js'
 import createEmotionCache from './createEmotionCache.js'
 
 const cache = createEmotionCache()
